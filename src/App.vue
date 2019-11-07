884e385bc678fb70c1ad8eee52aa8c139416cbfe
devitasari <devitas700@gmail.com>
2019-11-07 19:44:16 +0700

    login,register

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-
+    <login-register></login-register>
   </div>
 </template>
 
 <script>
 import Home from './Views/Home'
+import LoginRegister from './Views/LoginRegister'
 export default {
-    
+    name : 'App',
+    components : {
+      Home,
+      LoginRegister
+    }
 }
 </script>
 
